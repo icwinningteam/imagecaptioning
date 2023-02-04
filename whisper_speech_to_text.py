547a22b4ea8f0d1ca3837401e60178de0b99d8a4
pc2122 <pc2122@ic.ac.uk>
2023-02-04 21:23:34 +0000

    Fixed translation for audio to text

--- whisper_speech_to_text.py
+++ whisper_speech_to_text.py
@@ -2,28 +2,27 @@ import whisper
 from pytube import YouTube
 import os
 
-# url = "https://www.youtube.com/watch?v=S5FyS7tQuUw"
-try:
-    youtube = YouTube(url)
+# # url = "https://www.youtube.com/watch?v=S5FyS7tQuUw"
+# try:
+#     youtube = YouTube(url)
 
-    # extract only audio
-    vid = youtube.streams.filter(only_audio=True).first()
+#     # extracting audio
+#     vid = youtube.streams.filter(only_audio=True).first()
 
-    # check for destination to save file
-    destination = "."
+#     destination = "."
 
-    # download the file
-    out_file = vid.download(output_path=destination)
+#     # downloading 
+#     out_file = vid.download(output_path=destination)
 
-    # save the file
-    base, ext = os.path.splitext(out_file)
-    new_file = base + ".mp3"
-    os.rename(out_file, new_file)
-    print(youtube.title + " has been successfully downloaded.")
-except:
-    print("No URL passed!")
 
-# result of success
+#     base, ext = os.path.splitext(out_file)
+#     new_file = base + ".mp3"
+#     os.rename(out_file, new_file)
+#     print(youtube.title + " has been successfully downloaded.")
+# except:
+#     print("No URL passed!")
+
+
 
 
 def beautify(text):
@@ -37,5 +36,15 @@ def beautify(text):
 
 model = whisper.load_model("base")
 audio = whisper.load_audio("test_audio/french_audio.mp3")
-result = model.transcribe(audio, fp16=False, task="translate")
+
+# testing to consider translation
+audio_test_language = whisper.pad_or_trim(audio)
+mel = whisper.log_mel_spectrogram(audio_test_language).to(model.device)
+_, probs = model.detect_language(mel)
+language = {max(probs, key=probs.get)}
+
+if(language == "en"):
+    result = model.transcribe(audio, fp16=False)
+else:
+    result = model.transcribe(audio, fp16=False, task="translate")
 print(beautify(result["text"]))
