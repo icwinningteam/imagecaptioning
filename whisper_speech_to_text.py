7765566a9c5d2ac1477534b5bdb318fd2596d50c
Who23 <contactwho23@gmail.com>
2023-02-04 22:17:26 +0000

    finish up whisper

--- whisper_speech_to_text.py
+++ whisper_speech_to_text.py
@@ -1,42 +1,41 @@
 import whisper
 from pytube import YouTube
 import os
-  
-url = "https://www.youtube.com/watch?v=S5FyS7tQuUw"
+
+# url = "https://www.youtube.com/watch?v=S5FyS7tQuUw"
 try:
     youtube = YouTube(url)
+
+    # extract only audio
+    vid = youtube.streams.filter(only_audio=True).first()
+
+    # check for destination to save file
+    destination = "."
+
+    # download the file
+    out_file = vid.download(output_path=destination)
+
+    # save the file
+    base, ext = os.path.splitext(out_file)
+    new_file = base + ".mp3"
+    os.rename(out_file, new_file)
+    print(youtube.title + " has been successfully downloaded.")
 except:
     print("No URL passed!")
-  
-# extract only audio
-vid = youtube.streams.filter(only_audio=True).first()
-  
-# check for destination to save file
-destination = '.' 
-  
-# download the file
-out_file = vid.download(output_path=destination)
-  
-# save the file
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
-os.rename(out_file, new_file)
-  
-# result of success
-print(youtube.title + " has been successfully downloaded.")
 
+# result of success
 
 
 def beautify(text):
     words = text.split()
-    delimiters = ['.','!','?']
+    delimiters = [".", "!", "?"]
     for i in range(len(words)):
-        if(words[i][-1] in delimiters):
-            words[i] = words[i] + '\n'
-    return ' '.join(words)
+        if words[i][-1] in delimiters:
+            words[i] = words[i] + "\n"
+    return " ".join(words)
 
 
 model = whisper.load_model("base")
-audio = whisper.load_audio("test_audio/audio.mp3")
-result = model.transcribe(audio, fp16 = False)
-print(beautify(result['text']))
+audio = whisper.load_audio("test_audio/french_audio.mp3")
+result = model.transcribe(audio, fp16=False, task="translate")
+print(beautify(result["text"]))
