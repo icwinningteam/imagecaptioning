a84875688b99899b5fbb92fdd6b2c9760712d35c
Who23 <contactwho23@gmail.com>
2023-02-05 05:38:27 +0000

    add settings sync & chrome storage api

--- browser-extension/src/pages/popup/popup.js
+++ browser-extension/src/pages/popup/popup.js
@@ -1,41 +1,79 @@
-document.getElementById('switch1').addEventListener('change', function () {
-    // Add function
-    var checkBox = document.getElementById('switch1');
+const switch1 = document.getElementById("switch1");
+const slider1 = document.getElementById("slider1");
+const switch2 = document.getElementById("switch2");
+const switch3 = document.getElementById("switch3");
+const switch4 = document.getElementById("switch4");
+const switch5 = document.getElementById("switch5");
 
-    localStorage.setItem('textToSpeech', checkBox.checked);
+
+chrome.storage.local.get(['textToSpeech']).then(
+    (result) => {
+        switch1.checked = result.textToSpeech;
+        switch1.disabled = false;
+    }
+);
+
+chrome.storage.local.get(['speed']).then(
+    (result) => {
+        slider1.checked = result.speed;
+        slider1.disabled = false;
+    }
+);
+
+chrome.storage.local.get(["imageCaptioning"]).then(
+    (result) => {
+        switch2.checked = result.imageCaptioning;
+        switch2.disabled = false;
+    }
+);
+
+chrome.storage.local.get(['readSummary']).then(
+    (result) => {
+        switch3.checked = result.readSummary;
+        switch3.disabled = false;
+    }
+);
+
+chrome.storage.local.get(['easyReader']).then(
+    (result) => {
+        switch4.checked = result.textToSpeech;
+        switch4.disabled = false;
+    }
+);
+
+chrome.storage.local.get(['audioToText']).then(
+    (result) => {
+        switch5.checked = result.audioToText;
+        switch5.disabled = false;
+    }
+);
+
+switch1.addEventListener('change', function () {
+    // Add function
+    chrome.storage.local.set({ 'textToSpeech': switch1.checked });
 });
 
-document.getElementById('slider1').addEventListener('change', function () {
+slider1.addEventListener('change', function () {
     // Add function
-    var checkBox = document.getElementById('slider1');
-
-    localStorage.setItem('speed', checkBox.checked);
+    chrome.storage.local.set({ 'speed': slider1.checked });
 });
 
-document.getElementById('switch2').addEventListener('change', function () {
+switch2.addEventListener('change', function () {
     // Add function
-    var checkBox = document.getElementById('switch2');
-
-    localStorage.setItem('imageCaptioning', checkBox.checked);
+    chrome.storage.local.set({ 'imageCaptioning': switch2.checked });
 });
 
-document.getElementById('switch3').addEventListener('change', function () {
+switch3.addEventListener('change', function () {
     // Add function
-    var checkBox = document.getElementById('switch3');
-
-    localStorage.setItem('readSummary', checkBox.checked);
+    chrome.storage.local.set({ 'readSummary': switch3.checked });
 });
 
-document.getElementById('switch4').addEventListener('change', function () {
+switch4.addEventListener('change', function () {
     // Add function
-    var checkBox = document.getElementById('switch4');
-
-    localStorage.setItem('easyReader', checkBox.checked);
+    chrome.storage.local.set({ 'easyReader': switch4.checked });
 });
 
-document.getElementById('switch5').addEventListener('change', function () {
+switch5.addEventListener('change', function () {
     // Add function
-    var checkBox = document.getElementById('switch5');
-
-    localStorage.setItem('audioToText', checkBox.checked);
+    chrome.storage.local.set({ 'audioToText': switch5.checked });
 });
