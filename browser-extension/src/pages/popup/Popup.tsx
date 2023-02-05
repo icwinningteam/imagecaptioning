c31c68033c52b029afdac7e1a9a47ca5c177ef39
Ben Wong <benjaminwg1028@gmail.com>
2023-02-05 01:06:41 +0000

    Ben

--- browser-extension/src/pages/popup/Popup.tsx
+++ browser-extension/src/pages/popup/Popup.tsx
@@ -1,25 +1,72 @@
 import React from "react";
 
-import logo from "@assets/img/logo.svg";
+// import logo from "@assets/img/logo.svg";
+
+import "@pages/options/index.css";
 
 export default function Popup(): JSX.Element {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
-    </div>
+    <><link href="index.css" rel="stylesheet" type="text/css" /><script type="module" src="index.js"></script><div className="container">
+      <div id="header">EasyWeb</div>
+      <div className="component">
+        <div>Text to Speech</div>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
+
+      <div className="component">
+        <div>Voice</div>
+        <select name="cars" id="cars">
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+
+
+      <div className="component">
+        <div>Speed</div>
+        <div className="slidecontainer">
+          <input type="range" min="1" max="100" value="50" className="slider" id="myRange" />
+        </div>
+      </div>
+
+      <div className="component">
+        <div>Image Captioning</div>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
+      <div>
+
+
+        <div className="component">
+          <div>Read Summary</div>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+
+
+        <div className="component">
+          <div>Easy Reader</div>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+
+        <div className="component">
+          <div>Audio to Text</div>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+      </div>
+    </div></>
   );
 }
