ca75effa2a67c63ae1d7053a6a93387ad0513839
Theo Akande <ta722@ic.ac.uk>
2023-02-05 00:05:54 +0000

    added tts background function

--- browser-extension/src/pages/background/index.js
+++ browser-extension/src/pages/background/index.js
@@ -1,1 +1,20 @@
 console.log("Service Worker 👋");
+
+
+// for tts
+// Check if the browser supports the speechSynthesis API
+if ('speechSynthesis' in window) {
+    // Function to handle the text to speech
+    function readJSON(jsonData) {
+      // Use the text to speech API to read the JSON data
+      const utterance = new SpeechSynthesisUtterance(jsonData);
+      window.speechSynthesis.speak(utterance);
+    }
+
+    // Get the JSON data from the content script
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      if (request.action === 'readJSON') {
+        readJSON(request.jsonData);
+      }
+    });
+  }
